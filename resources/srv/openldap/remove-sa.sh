c8fa3b88a99887afed50f4479ff9181de9869e80
Simon Klein <simon.klein@cloudogu.com>
2020-11-23 16:40:46 +0100

    #10 Add remove script for ldap service account

---
+++ resources/srv/openldap/remove-sa.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+# variables which are used while rendering templates are exported
+
+TYPE="$1"
+SERVICE="$2"
+
+if [ X"${SERVICE}" = X"" ]; then
+    SERVICE="${TYPE}"
+    TYPE=""
+fi
+
+if [ X"${SERVICE}" = X"" ]; then
+    echo "usage create-sa.sh servicename"
+    exit 1
+fi
+
+OU="Bind Users"
+if [ X"${TYPE}" = X"rw" ]; then
+    OU="Special Users"
+fi
+export OU
+
+LDAP_DOMAIN=$(doguctl config --global domain)
+export LDAP_DOMAIN
+# proposal: use doguctl config openldap_suffix in future
+OPENLDAP_SUFFIX="dc=cloudogu,dc=com"
+export OPENLDAP_SUFFIX
+
+OPENLDAP_SUFFIX=$(doguctl config openldap_suffix --default "dc=cloudogu,dc=com")
+
+for result in $(ldapsearch -x -b "${OPENLDAP_SUFFIX}" "(cn=${SERVICE}*)" |grep '^cn' |grep -o "^cn:[ ]${SERVICE}[_].\{6\}$" |sed 's/cn:[ ]//g')
+do
+  export USERNAME="${result}"
+  doguctl template /srv/openldap/remove-user.ldif.tpl /srv/openldap/remove-user_"${USERNAME}".ldif
+  FILE="/srv/openldap/remove-user_${USERNAME}.ldif"
+  echo "Removing ldap user '${USERNAME}'"
+  ldapmodify -f "${FILE}"
+  echo "Removing temporary file '${FILE}'"
+  rm "${FILE}"
+done
